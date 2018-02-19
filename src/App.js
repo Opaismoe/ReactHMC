995681e11554b49df56c36fc82bd98990c4c5722
opaismoe <wiebe.buursink@gmail.com>
2018-02-19 19:52:11 +0100

    Add logo and sign in to navigation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,57 @@ import muiTheme from './styles/theme'
 import Navigation from './components/UI/Navigation'
 import Routes from './routes'
 
+const TitleFont =
+  '"Bolts", Franklin Gothic Medium, Franklin Gothic, ITC Franklin Gothic, -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif'
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiChip: {
+      root: {
+        background: 'linear-gradient(45deg, #ff1227 30%, #ff1227 90%)',
+        borderRadius: 12,
+        color: 'white',
+        height: 32,
+        padding: '0 10px',
+        boxShadow: '0 2px 3px 1px rgba(38, 38, 38, 0.2)'
+      }
+    }
+  },
+  palette: {
+    primary: {
+      light: '#bedee8',
+      main: '#0081aa',
+      dark: '#0c2830',
+      contrastText: '#b1ac7a'
+    },
+    secondary: {
+      light: '#ffdc60',
+      main: '#e6a435',
+      dark: '#ab8500',
+      contrastText: '#000000'
+    }
+  },
+  typography: {
+    fontFamily:
+      '"Roboto, "Helvetica Neue", Arial, sans-serif',
+    headline: {
+      fontFamily: TitleFont,
+      color: 'rgba(0, 0, 0, 0.87)',
+      fontWeight: 400
+    },
+    display1: {
+      fontFamily: TitleFont,
+      color: 'rgba(0, 0, 0, 0.87)',
+      fontWeight: 400
+    },
+    display2: {
+      fontFamily: TitleFont,
+      color: 'rgba(0, 0, 0, 0.87)',
+      fontWeight: 400
+    }
+  }
+})
+
 class App extends Component {
   static childContextTypes = {
     muiTheme: PropTypes.object.isRequired,
@@ -18,7 +69,7 @@ class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
+      <MuiThemeProvider theme={theme}>
         <Reboot>
           <div className="App">
             <Navigation />
--- src/components/UI/Navigation.js
+++ src/components/UI/Navigation.js
@@ -4,8 +4,19 @@ import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import signOut from '../../actions/user/sign-out'
 import AppBar from 'material-ui/AppBar'
+import Typography from 'material-ui/Typography'
+import Toolbar from 'material-ui/Toolbar'
 import IconButton from 'material-ui/IconButton'
 import Button from 'material-ui/Button'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+};
 
 class Navigation extends PureComponent {
   static propTypes = {
@@ -27,16 +38,30 @@ class Navigation extends PureComponent {
     this.props.push('/')
   }
 
+  signIn = () => {
+    this.props.push('/sign-in')
+  }
+
   render() {
     const { signedIn } = this.props
     return (
+      <div style={styles.root}>
       <AppBar position='static' >
-
-        {signedIn ?
-          <Button onClick={this.signOut.bind(this)}>Sign out</Button> :
-          <Button onClick={this.signUp}>Sign up</Button>
-        }
+        <Toolbar>
+          <Typography variant="headline" color="inherit" style={styles.flex}>
+            HMS
+          </Typography>
+          <Typography variant="p" color="inherit" style={styles.flex}>
+            Household Managment System
+          </Typography>
+          <Button style={{float:'right'}} onClick={this.signIn.bind(this)}>Sign in</Button>
+          {signedIn ?
+            <Button style={{float:'right', width:100}} onClick={this.signOut.bind(this)}>Sign out</Button> :
+            <Button style={{float:'right', width:100}} onClick={this.signUp}>Sign up</Button>
+          }
+        </Toolbar>
       </AppBar>
+      </div>
     )
   }
 }
