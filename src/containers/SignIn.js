688131ac1a4a4daf1dafebd12e441f02410e6a17
opaismoe <wiebe.buursink@gmail.com>
2018-02-19 18:29:11 +0100

    Fix SignIn with validate-js

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -6,25 +6,24 @@ import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
 import Button from 'material-ui/Button'
 import signIn from '../actions/user/sign-in'
-import Title from '../components/UI/Title'
+import { FormControl, FormHelperText } from 'material-ui/Form'
+import Typography from 'material-ui/Typography'
+import validate from 'validate.js'
 
 const dialogStyle = {
   width: '400px',
   margin: '50px auto',
-  padding: '2rem',
-}
-
-const buttonStyle = {
-  float: 'right',
-  marginLeft: '2rem',
+  padding: '2rem'
 }
 
 export class SignIn extends PureComponent {
   static propTypes = {
     push: PropTypes.func.isRequired,
     signIn: PropTypes.func.isRequired,
-    signedIn: PropTypes.bool,
+    signedIn: PropTypes.bool
   }
+
+  state = {}
 
   componentWillMount() {
     const { replace, signedIn } = this.props
@@ -33,45 +32,129 @@ export class SignIn extends PureComponent {
 
   submitForm(event) {
     event.preventDefault()
+
+    if (!this.validateAll()) return null
+
     const user = {
-      email: this.refs.email.getValue(),
-      password: this.refs.password.getValue(),
+      email: this.state.email,
+      password: this.state.password
     }
     this.props.signIn(user)
   }
 
-  signUp() {
-    this.props.push('/sign-up')
+  validateAll() {
+    return (
+      this.validateEmail(this.state.email) &&
+      this.validatePassword(this.state.password)
+    )
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    })
+    switch (name) {
+      case 'email':
+        this.validateEmail(event.target.value)
+        break
+      case 'password':
+        this.validatePassword(event.target.value)
+        break
+      default:
+        return false
+    }
+  }
+
+  validateEmail(email) {
+    const validationMsg = validate.single(email, {
+      presence: true,
+      email: true
+    })
+
+    if (!!validationMsg) {
+      this.setState({
+        emailError: validationMsg
+      })
+      return false
+    }
+
+    this.setState({
+      emailError: null
+    })
+    return true
+  }
+
+  validatePassword(password) {
+    const validationMsg = validate.single(password, {
+      presence: true,
+      length: {
+        minimum: 6,
+        message: 'must be at least 6 characters'
+      }
+    })
+
+    if (!!validationMsg) {
+      this.setState({
+        passwordError: validationMsg
+      })
+      return false
+    }
+
+    this.setState({
+      passwordError: null
+    })
+    return true
   }
 
   render() {
     return (
-      <Paper style={ dialogStyle }>
-        <Title content="Sign In" level={2} />
-
+      <Paper style={dialogStyle}>
+        <Typography component="h1" type="headline" style={{ marginBottom: 8 }}>
+          Sign in
+        </Typography>
         <form onSubmit={this.submitForm.bind(this)}>
-          <div className="input">
-            <TextField ref="email" type="email" hintText="Email address" />
-          </div>
-          <div className="input">
-            <TextField ref="password" type="password" hintText="Password"  />
-          </div>
+          <FormControl fullWidth className="formControl">
+            <TextField
+              id="email"
+              type="email"
+              placeholder="Email address"
+              error={!!this.state.emailError}
+              onChange={this.handleChange('email')}
+              fullWidth={true}
+            />
+            <FormHelperText
+              style={{ marginBottom: 6, marginTop: 6 }}
+              id="email-error-text"
+            >
+              {this.state.emailError}
+            </FormHelperText>
+          </FormControl>
+          <FormControl fullWidth className="formControl">
+            <TextField
+              id="password"
+              type="password"
+              placeholder="Password"
+              error={!!this.state.passwordError}
+              autoComplete="current-password"
+              onChange={this.handleChange('password')}
+              fullWidth={true}
+            />
+            <FormHelperText
+              style={{ marginBottom: 6, marginTop: 6 }}
+              id="password-error-text"
+            >
+              {this.state.passwordError}
+            </FormHelperText>
+          </FormControl>
+          <Button type="submit" raised color="primary">{'Sign in'}</Button>
         </form>
-        <Button
-          onClick={ this.signUp.bind(this) }
-          label="Sign up" />
-        <Button
-          style={ buttonStyle }
-          onClick={ this.submitForm.bind(this) }
-          label="Sign in"
-          primary={true} />
       </Paper>
     )
   }
 }
 
-const mapStateToProps = ({ currentUser }) => ({
-  signedIn: !!currentUser && !!currentUser._id,
+const mapStateToProps = ({ user }) => ({
+  signedIn: !!user && !!user._id
 })
 
 export default connect(mapStateToProps, { signIn, replace, push })(SignIn)
