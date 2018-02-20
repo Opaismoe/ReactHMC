ea9733532ed2d6f06284c5eff17feacdb54d0d29
Wiebe Buursink <wiebe.buursink@gmail.com>
2018-02-20 19:51:08 +0100

    Refactored add action for api

--- src/actions/grocerys/add.js
+++ src/actions/grocerys/add.js
@@ -1,11 +1,48 @@
+import API from '../../api/client'
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  LOAD_ERROR,
+  LOAD_SUCCESS
+} from '../loading'
+
+const api = new API()
+
+
 let nextGroceryId = 0
-export const addGrocery = text => {
-  return {
-    type: 'ADD_GROCERY',
-    id: nextGroceryId++,
-    text
+
+export const ADD_GROCERY = 'ADD_GROCERY'
+
+export const addGrocery = () => {
+  return (dispatch) => {
+    dispatch({ type: APP_LOADING })
+
+    api.post('/grocerys', {})
+      .then(() => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
+        })
+      })
   }
 }
+
+
+
+
+//
+// export const addGrocery = text => {
+//   return {
+//     type: 'ADD_GROCERY',
+//     id: nextGroceryId++,
+//     text
+//   }
+// }
 
 export const setVisibilityFilter = filter => {
   return {
